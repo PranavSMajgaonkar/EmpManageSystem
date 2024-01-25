189451e7fed742b3dc4f5091c9c3aab7ff007a48
PranavSMajgaonkar <pranavmajgaonkar123@gmail.com>
2024-01-25 07:45:43 +0530

    is record already present or not with enhanced loop

--- src/main/java/com/ssm/EmpManageSystem/service/Employee.java
+++ src/main/java/com/ssm/EmpManageSystem/service/Employee.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,22 +20,39 @@ public class Employee {
 
     public boolean isPresent(List<EmployeeEnt> employeeEnt){
         boolean noRecord = true;
-        List<EmployeeEnt> elist = null;
+//        List<EmployeeEnt> elist = null;
         //list of emails from body
-        List<String> email = employeeEnt.stream()
+        List<EmployeeEnt> newList = new java.util.ArrayList<>(employeeEnt.stream().toList());
+        /*List<String> email = employeeEnt.stream()
                 .map(EmployeeEnt::getEmail)
-                .toList();
+                .toList();*/
         //list of emails which is present in record
-        for (String e : email)
-            elist = employeeRepository.findByEmail(e);
+        List<EmployeeEnt> existList = employeeRepository.findAll();
+        if (!newList.isEmpty()) {
+            for (EmployeeEnt newOne : newList) {         //new 2 records
+                for (EmployeeEnt existOne : existList) { //existing 10 records
+                    if (existOne.getFirstName().equals(newOne.getFirstName())
+                            && existOne.getLastName().equals(newOne.getLastName())
+                            && existOne.getEmail().equals(newOne.getEmail())) {
+                        noRecord = false;
+                        break;
+                    } else {
+                        noRecord = true;
+                    }
+                }
+                newList.remove(newOne);
+            }
+        }
+//        for (String e : email)
+//            elist = employeeRepository.findByEmail(e);
 
         //iterate on both list and check is record present in db using email. if no save record else don't save
-        assert elist != null;
+        /*assert elist != null;
         for (EmployeeEnt existingOne : elist) {
             for (String newOne : email)
                 if(newOne.equals(existingOne.getEmail()))
                     return noRecord = false;
-        }
+        }*/
         return noRecord;
     }
     public void saveEmployee(EmployeeEnt employeeEnt){
