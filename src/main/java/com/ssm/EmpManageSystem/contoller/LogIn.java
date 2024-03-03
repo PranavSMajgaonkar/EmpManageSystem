8155fc3dd822f792e25be49396f9b692772f9416
PranavSMajgaonkar <pranavmajgaonkar123@gmail.com>
2024-03-04 03:56:43 +0530

    simple print statement

--- src/main/java/com/ssm/EmpManageSystem/contoller/LogIn.java
+++ src/main/java/com/ssm/EmpManageSystem/contoller/LogIn.java
@@ -5,4 +5,9 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LogIn {
+
+    @GetMapping("printMessage")
+    public String  print(){
+        return "This is login controller";
+    }
 }
